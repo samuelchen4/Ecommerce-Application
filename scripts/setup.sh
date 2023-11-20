45dbae64f9758f265c1adb0419531d3fcebe8972
Samuel Chen <sam_chen4@outlook.com>
2023-11-20 14:02:50 -0700

    add sudo to scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set up the application environment and dependencies after installation
-echo "Setting up the application after installation..."
+sudo echo "Setting up the application after installation..."
 
 # Install Node.js dependencies (if needed)
 # Example: Run npm install for the server
-npm install
+sudo npm install
