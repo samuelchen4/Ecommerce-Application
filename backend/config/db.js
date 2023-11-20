5ab4828ff300cec7708e91a57a22c3b233b5b916
Samuel Chen <sam_chen4@outlook.com>
2023-11-20 15:10:04 -0700

    fixed script

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,14 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      //   useCreateIndex: true,
-    });
+    const conn = await mongoose.connect(
+      'mongodb+srv://samuel888:Nigger321zz@ecommerceproject.onx7vup.mongodb.net/?retryWrites=true&w=majority',
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        //   useCreateIndex: true,
+      }
+    );
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.underline.cyan);
   } catch (err) {
@@ -14,5 +17,20 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI, {
+//       useUnifiedTopology: true,
+//       useNewUrlParser: true,
+//       //   useCreateIndex: true,
+//     });
+
+//     console.log(`MongoDB Connected: ${conn.connection.host}`.underline.cyan);
+//   } catch (err) {
+//     console.error(`Error: ${err.message}`.red.underline.bold);
+//     process.exit(1);
+//   }
+// };
 
 export default connectDB;
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,7 +4,7 @@
 echo "Starting the Node.js server..."
 pwd
 # Change directory to the Node.js server directory
-cd backend
+cd /home/ubuntu/app/backend
 
 # Start the Node.js server (replace app.js with your server entry point)
 node server.js
