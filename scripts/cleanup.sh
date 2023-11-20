ba1ea7faba47edc086e9be39ee2c71b47fa7ce56
Samuel Chen <sam_chen4@outlook.com>
2023-11-20 13:39:48 -0700

    building code pipeline test

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,1 +1,6 @@
-npm install
+##!/bin/bash
+
+# Clean up previous deployment artifacts or perform any pre-installation cleanup tasks
+echo "Performing cleanup tasks before installation..."
+# Example: Remove existing node_modules folder
+# rm -rf /home/ubuntu/app/node_modules
