891c513f1aa1af0f4bf85d107d2f065ab8baabda
Samuel Chen <sam_chen4@outlook.com>
2023-11-20 12:26:32 -0700

    building code pipeline test

---
+++ scripts/start_server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Start the Node.js server after installation and setup
+echo "Starting the Node.js server..."
+
+# Change directory to the Node.js server directory
+cd /app
+
+# Start the Node.js server (replace app.js with your server entry point)
+node backend/server.js
