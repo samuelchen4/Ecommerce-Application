8776480a216e1c6d3e30137ff64963f26d772457
Samuel Chen <sam_chen4@outlook.com>
2023-11-20 15:45:17 -0700

    changed server file

--- backend/server.js
+++ backend/server.js
@@ -33,13 +33,14 @@ app.get('/api/config/paypal', (req, res) =>
 );
 
 const __dirname = path.resolve();
+console.log(__dirname);
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+if ('production' === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '../../frontend/build/index.html'))
   );
 } else {
   app.get('/', (req, res) => {
